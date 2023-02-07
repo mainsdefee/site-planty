63d34f5008164858790ca1181bcf5609eeec6b2d
kv6060 <seblanco60@hotmail.com>
2023-02-07 04:02:00 +0100

    intégration des contenus sur WordPress

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -1,5 +1,39 @@
-<footer>
-          <a href="#">Mentions légales</a>
-    </footer>
-  </body>
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+/*Retirer les élments dates / hauts de page/copyr*/
+/*intégrer le liens mentions qui redirige vers la page mentions légales*/
+?>
+			<footer id="site-footer" class="header-footer-group">
+
+				<div class="section-inner">
+
+					 <a href="http://localhost/planty/mentions-legales/">Mentions légales</a>
+						
+						<?php
+						if ( function_exists( 'the_privacy_policy_link' ) ) {
+							the_privacy_policy_link( '<p class="privacy-policy">', '</p>' );
+						}
+						?>
+
+					</div><!-- .footer-credits -->
+						</span><!-- .to-the-top-short -->
+					</a><!-- .to-the-top -->
+
+				</div><!-- .section-inner -->
+
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
 </html>
--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -3,10 +3,21 @@
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent blankslate
+    // Chargement du style.css du thème parent twenty twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
  wp_enqueue_style( 'parent-style' , get_template_directory_uri() . '/style.css' );
     // Chargement du css/theme.css pour nos personnalisations
-    wp_enqueue_style( 'theme-style' , get_stylesheet_directory_uri() . '/css/theme.css' , array (), filemtime(get_stylesheet_directory() . '/css/theme.css' ));
+   wp_enqueue_style( 'theme-style' , get_stylesheet_directory_uri() . '/css/theme.css' , array (), filemtime(get_stylesheet_directory() . '/css/theme.css' ));
+//
+    // Gestion des menus
+function enregistre_mon_menu() {
+    register_nav_menu( 'menu_principal', __( 'Menu principal' ) );
 }
+add_action( 'init', 'enregistre_mon_menu' );
+}
+//ajouter une nouvelle zone de menu à mon thème
+function register_my_menu() {
+  register_nav_menu('footer-menu',__( 'Menu Footer' ));
+}
+add_action( 'init', 'register_my_menu' );
